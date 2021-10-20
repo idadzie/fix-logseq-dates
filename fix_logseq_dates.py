149322d079a3a1397eee2a98a11d8f28725aa52e
Isaac Dadzie <4581114+idadzie@users.noreply.github.com>
2021-10-20 12:16:12 +0000

    Add option to match any date
    Set the default behavior to match only linked date references.

--- fix_logseq_dates.py
+++ fix_logseq_dates.py
@@ -8,14 +8,24 @@ import dateparser
 import regex
 
 
-def replace_line(line_, date_format="%B %d, %Y"):
-    rgx = (
-        r"((?:\d{,2}\s)?(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|"
-        r"Sep|Oct|Nov|Dec)[a-z]*(?:-|\.|\s|,)\s?\d{,2}[a-z]*(?:-|,|\s)?\s?\d{2,4})"
+def sanitize_match(string):
+    return regex.sub(r"(\[\[|title[:]{,2}\s|\]\])", "", string.strip())
+
+
+def replace_line(line_, date_format="%B %d, %Y", any_date=False):
+    pattern = (
+        r"((?:\[\[|title[:]{,2}\s)?(?:\d{,2}\s)?(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|"
+        r"Sep|Oct|Nov|Dec)[a-z]*(?:-|\.|\s|,)\s?\d{,2}[a-z]*(?:-|,|\s)?\s?\d{2,4}?"
+        r"(?:\]\]|\s))"
     )
+    if any_date:
+        pattern = (
+            r"((?:\d{,2}\s)?(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|"
+            r"Sep|Oct|Nov|Dec)[a-z]*(?:-|\.|\s|,)\s?\d{,2}[a-z]*(?:-|,|\s)?\s?\d{2,4})"
+        )
 
     try:
-        match = regex.findall(rgx, line_)[0].strip()
+        match = sanitize_match(regex.findall(pattern, line_)[0])
     except (IndexError, AttributeError):
         return line_
 
@@ -24,7 +34,7 @@ def replace_line(line_, date_format="%B %d, %Y"):
     return line_.replace(match, date_string)
 
 
-def modify_file(path, date_format):
+def modify_file(path, date_format, any_date):
     with fileinput.FileInput(f"{path.absolute()}", inplace=True, backup=".bak") as f:
         for line in f:
             print(replace_line(line, date_format), end="")
@@ -78,6 +88,11 @@ def init_argparse():
         dest="graph_directory",
         help="absolute path to your local Logseq graph.",
     )
+    parser.add_argument(
+        "--any-date",
+        action="store_true",
+        help="match any date found not only referenced ones.",
+    )
     return parser
 
 
@@ -99,6 +114,8 @@ def main():
     if not logseq_path.is_dir():
         sys.exit("Kindly provide a valid logseq directory.")
 
+    any_date = args.any_date
+
     for path in logseq_path.iterdir():
         if path.name not in (args.journals_directory, args.pages_directory):
             continue
@@ -106,7 +123,7 @@ def main():
         for path_ in path.iterdir():
             if path_.is_file() and path_.suffix != ".bak":
                 try:
-                    modify_file(path_, date_format)
+                    modify_file(path_, date_format, any_date)
                     cleanup(path_)
                     # Broad! yes; don't @ me.
                 except Exception:  # noqa
